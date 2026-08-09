3d947b62c8ec47d6178487f33aba227268421b2d
McboyPutra05 <fahmiprismaputradani@gmail.com>
2026-08-09 22:51:28 +0700

    refactor: remove old jobs. route (replaced by /extraction)

--- src/routes/jobs.$jobId.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { ExtractionPage } from '@/pages/ExtractionPage'
-
-export const Route = createFileRoute('/jobs/$jobId')({
-  component: ExtractionPage,
-})
